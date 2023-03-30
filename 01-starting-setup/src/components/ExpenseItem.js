22bb36bb799c830fa3fd9c27d3b25bd5980c7519
Milos Damjanovic <milos.10001@yahoo.com>
2023-03-30 12:38:34 +0200

    Checkpoint 3.35

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(){
+
+    return (
+        <div className='expense-item'>
+            <div>April 23th 2021</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className=' expense-item__price'>$253.68</div>
+            </div>
+        </div>
+
+
+    )
+}
+
+
+export default ExpenseItem;
+
+
+
+
+
