201226f1a5de2a957244ffcf4bd9561745839b1f
Milos Damjanovic <milos.10001@yahoo.com>
2023-04-03 20:33:30 +0200

    Checkpoint  4.45

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,12 +1,60 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+
+  const expenses = [
+
+    {
+      id: 'e1',
+      amount: 210.23,
+      title: 'Car insurance',
+      date: new Date(2021, 7, 16)
+    },
+
+    {
+      id: 'e2',
+      amount: 167.3,
+      title: 'New TV',
+      date: new Date(2021, 4, 12)
+    },
+
+    {
+      id: 'e3',
+      amount: 750,
+      title: 'Flat rent',
+      date: new Date(2021, 8, 31)
+    },
+
+    {
+      id: 'e4',
+      amount: 880,
+      title: 'PS5',
+      date: new Date(2021, 9, 2)
+    }
+
+  ];
+
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses
+        items = {expenses}
+      />
     </div>
   );
 }
+
+/*
+  HOW WAS REACT RETURN STATEMENT WRITTEN BEFORE JSX, USING REACT OBJ
+  
+  React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, "Let's get started"),
+    React.createElement(Expenses, { items: expenses })
+  )
+
+*/
 
 export default App;
---
+++ 01-starting-setup/src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,24 @@
+import './ExpenseDate.css'
+
+const ExpenseDate = (props) => {
+
+
+    const month = props.date.toLocaleString('en-US', {month: 'long'})
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+    const year = props.date.getFullYear();
+
+
+    return(
+
+        <div className='expense-date'>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+
+    )
+
+}
+
+
+export default ExpenseDate;
